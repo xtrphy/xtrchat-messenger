55e6a1633135876a18f159a039620b7d034b2fe3
xtrphy <perepeckinvlad@gmail.com>
2025-05-17 21:57:19 +0500

    Stylize chat divide user's messages by color

--- client/src/components/ChatBody/ChatBody.jsx
+++ client/src/components/ChatBody/ChatBody.jsx
@@ -3,6 +3,8 @@ import styles from './ChatBody.module.css';
 import ChatInput from '../ChatInput/ChatInput';
 
 const ChatBody = ({ messages, selectedUserId }) => {
+    const userId = localStorage.getItem('userId');
+
     const formatTime = (time) => {
         const date = new Date(time);
         return date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
@@ -12,15 +14,32 @@ const ChatBody = ({ messages, selectedUserId }) => {
         <main className={styles.chatBodyContainer}>
             <div className={styles.backgroundImage}></div>
             <div className={styles.messagesContainer}>
-                {messages.map(message => (
-                    <div className={styles.message}>
-                        <span className={styles.messageContent}>
-                            {message.content}
-                        </span>
-                        <span className={styles.messageDate}>{formatTime(message.createdAt)}
-                        </span>
-                    </div>
-                ))}
+                {messages.map(message => {
+                    if (message.senderId === userId) {
+                        return (
+                            <div className={styles.userMessage}>
+                                <span className={styles.userMessageContent}>
+                                    {message.content}
+                                </span>
+                                <span className={styles.userMessageDate}>{formatTime(message.createdAt)}
+                                </span>
+                            </div>
+                        )
+                    }
+
+                    if (message.senderId !== userId) {
+                        return (
+                            <div className={styles.companionMessage}>
+                                <span className={styles.companionMessageContent}>
+                                    {message.content}
+                                </span>
+                                <span className={styles.companionMessageDate}>{formatTime(message.createdAt)}
+                                </span>
+                            </div>
+                        )
+                    }
+                }
+                )}
             </div>
             <ChatInput selectedUserId={selectedUserId} />
         </main>
