65053eb70458369deccbbe082c65546e3a321e3b
xtrphy <perepeckinvlad@gmail.com>
2025-05-18 18:46:22 +0500

    Edit allowedOrigins

--- server/app.js
+++ server/app.js
@@ -11,8 +11,7 @@ const botService = require('./services/botService.js');
 const app = express();
 
 const allowedOrigins = [
-    'http://localhost:5173',
-    'https://xtrchat-messenger.onrender.com'
+    'https://xtrmessenger.netlify.app'
 ];
 
 app.use(express.json());
