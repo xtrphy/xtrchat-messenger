946bc0e76f9f8cdad4a51eecaa875e8a9b591606
xtrphy <perepeckinvlad@gmail.com>
2025-05-18 18:25:40 +0500

    Add additional port

--- server/app.js
+++ server/app.js
@@ -49,5 +49,5 @@ app.use('/api/chats', chatsRouter);
 // Messages
 app.use('/api/messages', messagesRouter);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
